186c2baf2fa14cbfb4586655dc0f3dea99094153
Arthur Azevedo <aea@poli.br>
2025-03-30 21:06:45 -0300

    Codigo de normalização

--- libneo.cpp
+++ libneo.cpp
@@ -36,113 +36,58 @@
 
 /* ---------------------------------------------------------------------- */
 /* includes */
-#include <iostream> /* (Input/output library) Several standard stream objects */
-using namespace std;
-/* #include <cstdio> */ /* (Input/output library) C-style input-output functions */
-/* #include <fstream> */ /* (Input/output library) std::basic_fstream, std::basic_ifstream, std::basic_ofstream class templates and several typedefs */
-/* #include <iomanip> */ /* (Input/output library) Helper functions to control the format of input and output */
-/* #include <ios> */ /* (Input/output library) std::ios_base class, std::basic_ios class template and several typedefs */
-/* #include <iosfwd> */ /* (Input/output library) Forward declarations of all classes in the input/output library */
-/* #include <istream> */ /* (Input/output library) std::basic_istream class template and several typedefs */
-/* #include <ostream> */ /* (Input/output library) std::basic_ostream, std::basic_iostream class templates and several typedefs */
-/* #include <spanstream> */ /* (C++23, Input/output library) std::basic_spanstream, std::basic_ispanstream, std::basic_ospanstream class templates and typedefs */
-/* #include <sstream> */ /* (Input/output library) std::basic_stringstream, std::basic_istringstream, std::basic_ostringstream class templates and several typedefs */
-/* #include <streambuf> */ /* (Input/output library) std::basic_streambuf class template */
-/* #include <strstream> */ /* (deprecated in C++98, Input/output library) std::strstream, std::istrstream, std::ostrstream */
-/* #include <syncstream> */ /* (C++20, Input/output library) std::basic_osyncstream, std::basic_syncbuf, and typedefs */
 
-/* #include "libuser.hpp" */ /* your personal user library together with libuser.cpp */
-/* #include "libneo.hpp" */ /* To be created for this template if needed */
+#include "libneo.hpp" 
 
 /* ---------------------------------------------------------------------- */
-/* definitions */
+//Normalization function
 
-#ifndef VERSION /* g++ -DVERSION="0.1.160612.142306" */
-#define VERSION "20250330.141931" /* Version Number (string) */
-#endif
-
-/* Debug */
-#ifndef DEBUG /* g++ -DDEBUG=1 */
-#define DEBUG 0 /* Activate/deactivate debug mode */
-#endif
-
-#if DEBUG==0
-#define NDEBUG
-#endif
-/* #include <cassert> */ /* (Error handling) Conditionally compiled macro that compares its argument to zero */
-
-/* Debug message if DEBUG on */
-#define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
-
-/* limits */
-#define SBUFF 256 /* string buffer */
-
-/* ---------------------------------------------------------------------- */
-/* classes : better located at libxxx.h */
-
-class document
+void normalizar()
 {
-    public:
-        void help(void); /* print some help */
-        void copyr(void); /* print version and copyright information */
-};
+    double x, minimo, maximo;
 
-/* ---------------------------------------------------------------------- */
-/* methods */
+    // Coleta de dados
+    cout << "Digite o valor do numero a ser normalizado: ";
+    cin >> x;
 
-/* ---------------------------------------------------------------------- */
-/* Prints help information
- *  usually called by opt -h or --help
- */
-void document::help(void)
-{
-    IFDEBUG("help()");
-    /* printf("%s - %s\n", "libneo", "Biblioteca de funções"); */
-    cout << "libneo - Biblioteca de funções\n";
-    cout << "\nUsage: libneo\n\n";
-    cout << "This program does...\n";
-    /* add more stuff here */
-    cout << "\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n";
-    cout << "\nTodo:\n\tLong options not implemented yet.\n";
-    cout << "\nAuthor:\n\tWritten by Arthur Epifanio De Azevedo <aea@poli.br>\n\n";
-    return;
+    while(true)
+    {
+    cout << "Digite o valor mínimo: ";
+    cin >> minimo;
+    if(minimo >= x)
+    {
+        cerr << "Erro: minimo; deve ser menor que x." << endl;
+        continue;
+    }else
+        break;
+    }
+
+    while(true)
+    {
+    cout << "Digite o valor máximo: ";
+    cin >> maximo;
+    if (maximo <= x)
+    {
+        cerr << "Erro: maximo; deve ser maior que x." << endl;
+        continue;
+        
+    }else if (maximo <= minimo)
+    {
+        cerr << "Erro: maximo; deve ser maior que minimo." << endl;
+        continue;
+    }else
+        break;
+    }
+
+  
+    // Normalização
+    double resultado = (x - minimo) / (maximo - minimo);
+
+    // Exibição do resultado
+    cout << "O valor normalizado é: " << resultado << endl;
 }
 
-/* ---------------------------------------------------------------------- */
-/* Prints version and copyright information
- *  usually called by opt -V
- */
-void document::copyr(void)
-{
-    IFDEBUG("copyr()");
-    cout << "libneo - Version " << VERSION << "\n";
-    cout << "\nCopyright (C) 2025 Arthur Epifanio De Azevedo <aea@poli.br>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This  is  free  software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n";
-    return;
-}
 
-/* ---------------------------------------------------------------------- */
-/* This function does ...
- *
- * It receives as input ...
- *
- * It returns ...
- */
-int main(void)
-{
-    /* local declarations */
-    document doc; /* create an object of a class */
-    int i; /* general index */
-    char s[SBUFF]; /* a string to hold ... */
-
-    /* code */
-    doc.help();
-    doc.copyr();
-
-    /* ...and we are done */
-    return 0;
-}
-
-/* add more functions here */
 
 /* ---------------------------------------------------------------------- */
 /* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : CPP config - Vim modeline */
