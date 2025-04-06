7a6fe18b3e77a39f788ca22b644104eee211f168
Arthur Azevedo <aea@poli.br>
2025-04-06 19:38:38 -0300

    adiciona funcionalidade para gerar e ordenar um vetor de 500 números aleatórios

--- ex5.cpp
+++ ex5.cpp
@@ -37,88 +37,10 @@
 /* ---------------------------------------------------------------------- */
 /* includes */
 #include <iostream> /* (Input/output library) Several standard stream objects */
+#include <vector>  /* (Vector library) Standard template library for vectors */
+#include <algorithm> /* (Algorithm library) Standard template library for algorithms */
+#include <random> /* (Random library) Standard template library for random numbers */
 using namespace std;
-/* #include <cstdio> */ /* (Input/output library) C-style input-output functions */
-/* #include <fstream> */ /* (Input/output library) std::basic_fstream, std::basic_ifstream, std::basic_ofstream class templates and several typedefs */
-/* #include <iomanip> */ /* (Input/output library) Helper functions to control the format of input and output */
-/* #include <ios> */ /* (Input/output library) std::ios_base class, std::basic_ios class template and several typedefs */
-/* #include <iosfwd> */ /* (Input/output library) Forward declarations of all classes in the input/output library */
-/* #include <istream> */ /* (Input/output library) std::basic_istream class template and several typedefs */
-/* #include <ostream> */ /* (Input/output library) std::basic_ostream, std::basic_iostream class templates and several typedefs */
-/* #include <spanstream> */ /* (C++23, Input/output library) std::basic_spanstream, std::basic_ispanstream, std::basic_ospanstream class templates and typedefs */
-/* #include <sstream> */ /* (Input/output library) std::basic_stringstream, std::basic_istringstream, std::basic_ostringstream class templates and several typedefs */
-/* #include <streambuf> */ /* (Input/output library) std::basic_streambuf class template */
-/* #include <strstream> */ /* (deprecated in C++98, Input/output library) std::strstream, std::istrstream, std::ostrstream */
-/* #include <syncstream> */ /* (C++20, Input/output library) std::basic_osyncstream, std::basic_syncbuf, and typedefs */
-
-/* #include "libuser.hpp" */ /* your personal user library together with libuser.cpp */
-/* #include "ex5.hpp" */ /* To be created for this template if needed */
-
-/* ---------------------------------------------------------------------- */
-/* definitions */
-
-#ifndef VERSION /* g++ -DVERSION="0.1.160612.142306" */
-#define VERSION "20250406.193035" /* Version Number (string) */
-#endif
-
-/* Debug */
-#ifndef DEBUG /* g++ -DDEBUG=1 */
-#define DEBUG 0 /* Activate/deactivate debug mode */
-#endif
-
-#if DEBUG==0
-#define NDEBUG
-#endif
-/* #include <cassert> */ /* (Error handling) Conditionally compiled macro that compares its argument to zero */
-
-/* Debug message if DEBUG on */
-#define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
-
-/* limits */
-#define SBUFF 256 /* string buffer */
-
-/* ---------------------------------------------------------------------- */
-/* classes : better located at libxxx.h */
-
-class document
-{
-    public:
-        void help(void); /* print some help */
-        void copyr(void); /* print version and copyright information */
-};
-
-/* ---------------------------------------------------------------------- */
-/* methods */
-
-/* ---------------------------------------------------------------------- */
-/* Prints help information
- *  usually called by opt -h or --help
- */
-void document::help(void)
-{
-    IFDEBUG("help()");
-    /* printf("%s - %s\n", "ex5", "Ordenar Vetor"); */
-    cout << "ex5 - Ordenar Vetor\n";
-    cout << "\nUsage: ex5\n\n";
-    cout << "This program does...\n";
-    /* add more stuff here */
-    cout << "\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n";
-    cout << "\nTodo:\n\tLong options not implemented yet.\n";
-    cout << "\nAuthor:\n\tWritten by Arthur Epifanio De Azevedo <aea@poli.br>\n\n";
-    return;
-}
-
-/* ---------------------------------------------------------------------- */
-/* Prints version and copyright information
- *  usually called by opt -V
- */
-void document::copyr(void)
-{
-    IFDEBUG("copyr()");
-    cout << "ex5 - Version " << VERSION << "\n";
-    cout << "\nCopyright (C) 2025 Arthur Epifanio De Azevedo <aea@poli.br>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This  is  free  software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n";
-    return;
-}
 
 /* ---------------------------------------------------------------------- */
 /* This function does ...
@@ -129,16 +51,28 @@ void document::copyr(void)
  */
 int main(void)
 {
-    /* local declarations */
-    document doc; /* create an object of a class */
-    int i; /* general index */
-    char s[SBUFF]; /* a string to hold ... */
 
-    /* code */
-    doc.help();
-    doc.copyr();
+    // Initialize random number generator
+    random_device rd;  // Obtain a random number from hardware
+    mt19937 eng(rd()); // Seed the generator
+    uniform_int_distribution<> distr(1, 100); // Define the range
 
-    /* ...and we are done */
+    // Vetor com numeros 500 aleatorios
+    vector<int> numeros(500);
+    for (int i = 0; i < 500; ++i)
+    {
+        numeros.push_back(distr(eng)); // Preenche o vetor com numeros aleatorios
+    }
+
+    // Ordena o vetor
+    sort(numeros.begin(), numeros.end()); // Ordena o vetor em ordem crescente
+    cout << "Vetor ordenado: " << endl;
+    for(int i = 0; i < 500; ++i)
+    {
+        cout << numeros[i] << " "; // Imprime o vetor ordenado
+        if(i % 10 == 9) cout << endl; // Imprime uma nova linha a cada 10 numeros
+    }
+    
     return 0;
 }
 
